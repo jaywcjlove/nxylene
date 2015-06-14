0525e430eb9646e40aa7d621657a27cc17d9adc7
jaywcjlove <398188662@qq.com>
2015-06-14 23:29:50 +0800

    express 4.x静态资源使用目录新方法

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view options',{
 })
 
 //静态资源使用目录
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname+'/public'));
 
 // 定义好我们 app 的行为之后，让它监听本地的 3000 端口。
 // 这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
