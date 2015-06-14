64bea50e4419296e6ca46404f4a158ee1f2afc8c
jaywcjlove <398188662@qq.com>
2015-06-14 22:59:18 +0800

    伪造假数据，并展现。

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,32 @@ module.exports = function (app) {
     // request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
     // res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容
     app.get('/', function (req, res) {
-        res.render('index', { title: 'JSLite 官网' });
+        res.render('index', { 
+            title: 'JSLite 官网',
+            pic_list: [{
+                name: "长腿美女精美壁",
+                _id: 1,
+                url: "http://img2.tgbusdata.cn/v2/thumb/jpg/MTlGRCw1ODAsMTAwLDQsMywxLC0xLDAscms1MCw2MS4xNTIuMjQyLjEx/u/iphone.tgbus.com/UploadFiles/201506/2015061017310130.png"
+            },{
+                name: "长腿美女精美壁",
+                _id: 2,
+                url: "http://www.tgbus.com/image.html?url=http://iphone.tgbus.com/UploadFiles/201406/2014061911370889.jpg"
+            },{
+                name: "长腿美女精美壁",
+                _id: 3,
+                url: "http://iphone.tgbus.com/UploadFiles/201506/2015060217560183.png"
+            },{
+                name: "长腿美女精美壁",
+                _id: 4,
+                url: "http://i.bufan.com/upload/2014-07-09/14048768403033.jpg"
+            },{
+                name: "长腿美女精美壁",
+                _id: 5,
+                url: "http://i.bufan.com/upload/2014-06-24/14035889302261.jpg"
+            }]
+        });
     });
-    app.get('/movie/:id', function (req, res) {
+    app.get('/pic/:id', function (req, res) {
         res.render('detail', { title: '详情页面' });
     });
 }
