a32fa9b04b371e145fb8c1bb86295ced1b9b38bc
jaywcjlove <398188662@qq.com>
2015-07-01 01:53:14 +0800

    于我们编辑页面添加样式

--- themes/default/source/js/contact.js
+++ themes/default/source/js/contact.js
@@ -12,6 +12,22 @@
         console.log("message:2:",marked(con_str));
     })
 
+    
+    $(function(){
+        $('.JSLite_tabs').children().each(function(idx,item){
+            var cla = $(item).attr('class');
+            cla&&cla.indexOf('JSLite_tabHead')>-1?$(item).children().each(function(n,obj){
+                $(obj).bind('click',function (d){
+                    $(item).children().removeClass("on");
+                    $(this).addClass("on");
+                    $(item).next().children().map(function(nn,nobj){
+                        nn==n?$(nobj).show():$(nobj).hide();
+                    })
+                })
+            }):null;
+        })
+    })
+
 })(JSLite)
 
 
