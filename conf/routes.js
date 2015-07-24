c7b12dba02b906d00ba7d1f753351aec6ee4fa36
jaywcjlove <398188662@qq.com>
2015-07-24 18:59:54 +0800

    wwwdddsdsdsdsdw

--- conf/routes.js
+++ conf/routes.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
     // 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
     // request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
     // res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容
-
+    
     //对用户的预处理
     app.use(function(req,res,next){
         var _user,reg = new RegExp(/^\/admin.*/);
